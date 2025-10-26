df9e1f6111fdd266be413262158ef32a374bdc1a
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2025-10-26 19:40:39 +0100

    Commit 2: Teilaufgabe 2

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -108,5 +108,45 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * Loesung zu Teilaufgabe 02: Erster Test
+     */
+
+    @Test
+    @DisplayName("should only clear screen but keep stored value after first clear press")
+    void testSingleClearKeepsStoredValue() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(8);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressClearKey(); // sollte nur den Bildschirm löschen
+
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey(); // erwartet 8 + 3 = 11
+
+        String expected = "11";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Loesung zu Teilaufgabe 02: Zweiter Test
+     */
+
+    @Test
+    @DisplayName("should do nothing when equals key is pressed without prior operation")
+    void testEqualsWithoutOperationDoesNothing() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(9);
+        calc.pressEqualsKey(); // sollte nichts tun
+
+        String expected = "9";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
